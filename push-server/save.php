7a1549de22934307beb692a5bad5a3591abf393b
André Rodrigues <andre@ateliedepropaganda.com.br>
2017-08-16 18:31:12 -0300

    first commit

--- push-server/save.php
+++ push-server/save.php
@@ -1,9 +1,13 @@
 <?php
 
+$endpoint = $_POST["endpoint"];
+
 header('Access-Control-Allow-Origin: *');
 
 $handle = fopen("endpoints.txt", "a") or die("Unable to open file!");
 
-fwrite($handle, $_POST["endpoint"] . "\n");
+print_r(fwrite($handle, $endpoint . "\n"));
 
 fclose($handle);
+
+header("location: endpoints.php");
