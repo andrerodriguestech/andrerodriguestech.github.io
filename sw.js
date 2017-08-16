ed9ab3439e22c1857d520439b599be5e8efa9164
André Rodrigues <andre@ateliedepropaganda.com.br>
2017-08-16 16:49:11 -0300

    first commit

---
+++ sw.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var CACHE_NAME = 'ml-cache-v1.01';
+
+var urlsToCache = [
+  'public/images/badge.png',
+  'public/images/launcher-icon-1x.png',
+  'public/images/launcher-icon-2x.png',
+  'public/images/launcher-icon-256x256.png',
+  'public/images/launcher-icon-4x.png',
+  'public/images/launcher-icon-512x512.png',
+  'public/images/launcher-icon-8x.png',
+  'public/scripts/main.js',
+  'public/styles/main.css',
+  'fav_icon.ico',
+  'public/images/icon.png',
+  'index.html',
+  'manifest.json',
+  'sw.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['ml-cache-v1.01'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('push', function(event) {
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Mundo Lenovo';
+  const options = {
+    body: `${event.data.text()}`,
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  const notificationPromise = self.registration.showNotification(title, options);
+  event.waitUntil(notificationPromise);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://andrerodriguestech.github.io/')
+  );
+});
